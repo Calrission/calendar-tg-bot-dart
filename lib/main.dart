f4c3396e3f4fb126ef265859f59f6c38a64dbbc6
Artemii Strukov <kavrese@gmail.com>
2025-06-13 23:36:15 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:televerse/televerse.dart';
+
+void main() {
+  Bot bot = Bot('YOUR_BOT_TOKEN');
+}
---
+++ test/my_calendar_tg_bot_dart_test.dart
@@ -0,0 +1,6 @@
+import 'package:my_calendar_tg_bot_dart/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+
+}
